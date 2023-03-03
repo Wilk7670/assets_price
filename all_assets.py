100b3f26c1fd05c8773966e9c85eaf6b5e56d253
Wiktor Grochowski <wiktorgrochowski@wp.pl>
2023-03-03 23:28:28 +0100

    Added functionality to store data of assets to be checked in separate assets_input.yaml file and to use that file to import data instead of storing it in code

--- all_assets.py
+++ all_assets.py
@@ -1,7 +1,9 @@
 import yfinance as yf
 import math
+import json
+import os
 
-####################################-----------------> FUNCTIONS START <-----------------####################################
+#################################################-----------------> FUNCTIONS START <-----------------#################################################
 # Based on size of input value this function uses rounding function with predefined significant digits of rounding. Rounded value is returned.
 def round_accordingly_to_value_size(value_to_be_rounded):
     if value_to_be_rounded > 1000:
@@ -37,40 +39,32 @@ def fetch_one_asset_type_price(dict_of_asset_type, asset_type):
             price = (asset_info['Close'][-1])
             dict_of_one_asset_type_prices[dict_of_asset_type[asset]] = price
         except Exception as e:
-            print(f'Error fetching data for {asset_type} {dict_of_asset_type[asset]} ---------> {str(e)}')
+            print(f"\n################\nError fetching data for {asset_type} {dict_of_asset_type[asset]} ---------> {str(e)}\n################\n")
     return dict_of_one_asset_type_prices
-####################################-----------------> FUNCTIONS END <-----------------####################################
+
+# This function import data from file by default named "assets_input.json" or other if it's given in function argument. If wanted file is stored in the same location as code file, it'll returns file content
+def import_data(name_of_file = "assets_input.json"):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, name_of_file)
+    print (dir_path)
+    print (file_path)
+    try:
+        print (file_path)
+        with open(file_path, "r", encoding="UTF-8") as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+            print(f"\n################\nError importing JSON data from {file_path} ---------> {str(e)}\n################\n")
 
 
-####################################------------------> INPUT START <------------------####################################
-# Dictionaries held on lowest level contain ticker of asset from yahoo finance as the key and it's readable version as it's value.
-big_list = [
-            {"stocks": 
-                    {"MBR.WA": "MOBRUK / PLN", 
-                    "DNP.WA": "DINO / PLN",
-                    "ETHW.SW": "ETH / CHF"}
-            },
-            {"fiat_currencies": 
-                    {"EUR=X": "USD / EUR",
-                     "CHFEUR=X": "CHF / EUR",
-                     "PLN=X": "USD / PLN", 
-                     "EURPLN=X": "EUR / PLN"}
-            },
-            {"crypto_currencies": 
-                    {"BTC-USD": "BTC / USD", 
-                     "ETH-USD": "ETH / USD", 
-                     "ATOM-USD": "ATOM / USD", 
-                     "DOT-USD": "DOT / USD", 
-                     "YLD8066-USD": "YLD / USD", 
-                     "BNB-USD": "BNB / USD"}
-            }
-           ]
-####################################-------------------> INPUT END <-------------------####################################
+#################################################------------------> INPUT START <------------------#################################################
+# It fetches list of dictionaries from YAML file
+big_list = (import_data())
 
-####################################-----------------> TRIGGER START <-----------------####################################
+
+#################################################-----------------> TRIGGER START <-----------------#################################################
 dict_of_assets_with_prices = (fetch_all_assets_prices(big_list))
 for dictionary in dict_of_assets_with_prices:
     print (f"----------------------- {dictionary} -----------------------")
     for assets_with_prices in dict_of_assets_with_prices[dictionary]:
        print (f"{assets_with_prices} ====== {round_accordingly_to_value_size(dict_of_assets_with_prices[dictionary][assets_with_prices])}")
-####################################------------------> TRIGGER END <------------------####################################
+
