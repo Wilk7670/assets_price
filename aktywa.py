e8fe38e2f45da3a593be02525f6c404c7d915fce
Wiktor Grochowski <wiktorgrochowski@wp.pl>
2023-03-09 22:46:03 +0100

    Code is now working. Still need to do some changes to make it better before merging

--- aktywa.py
+++ aktywa.py
@@ -23,63 +23,68 @@ def fetch_price(ticker):
     price = (asset_info['Close'][-1])
     return price
 
+# Based on size of input value this function uses rounding function with predefined significant digits of rounding. Rounded value is returned.
+def round_accordingly_to_value_size(value_to_be_rounded):
+    if value_to_be_rounded > 1000:
+        return int(value_to_be_rounded)
+    elif (value_to_be_rounded < 1000) and (value_to_be_rounded >= 100):
+        return (round_to_X_significant_digits(value_to_be_rounded, 5))
+    else:
+        return (round_to_X_significant_digits(value_to_be_rounded, 4))
 
-# Wpierw wyszukać wszystkie exchange rate i zrobić z nich słownik gdzie kluczem będzie np "USD/PLN" lub "PLN/CHF"
-# Potem wyszukać ceny a jeśli notowanie jest w innej alucie to zastosować exchange i koniec.
+# This function rounds given value to significant digit that is given in input. Rounded value is returned.
+def round_to_X_significant_digits(value_to_be_rounded, significant_digit):
+    if value_to_be_rounded == 0:
+        return 0
+    else:
+        return round(value_to_be_rounded, -int(math.floor(math.log10(abs(value_to_be_rounded)))) + (significant_digit - 1))
 
 
+
+
+
+# Import data fron input .json fies.
 main_dict = (import_data(standardize_file_path("input_assets_to_be_checked.json")))
 exchange_rates_dict_input = (import_data(standardize_file_path("input_exchange_rates.json")))
-wanted_output = (import_data(standardize_file_path("output_wanted.json")))
+wanted_output = (import_data(standardize_file_path("input-wanted_result.json")))
 
-print (main_dict)
-print ()
-print (exchange_rates_dict_input)
-print ()
-print (wanted_output)
-print ()
-
-
-
-# Tworzenie słownika z exchange rate
+# Fetching exchange rates into dictionary
 updated_exchange_rates_dict = {}
 for ex_rate in exchange_rates_dict_input:
     price = (fetch_price(exchange_rates_dict_input[ex_rate]))
     updated_exchange_rates_dict[ex_rate] = price
-print (updated_exchange_rates_dict)
 
-#Sprawanie ceny main dict
-updated_main_dict = {}
+# Fetching price for the rest of assets and merging it with a exchange rates dictionary. That part covers scenario_1 where wanted output 
+# can be simply fetched from yahoo finance and scenario_2 where price fetched from yahoo finance need to be recalculated using exchange rate. 
+updated_main_dict = updated_exchange_rates_dict.copy()
 for item in wanted_output:
-                                                                # rozdzielenie na PATH_1 i PATH_2 w zależności czy robić exchange czy nie
-                                                                # PATH_1 -------- jeśli nie trzeba exchange---------------------------------------------------------------- PATH_1
+    # scenario_1
     if item in main_dict:
         price = (fetch_price(main_dict[item]))
         updated_main_dict[item] = price
-                                                                # PATH_2 ------ jeśli potrzeba zrobić exchange------------------------------------------------------------- PATH_2
+    # scenario_2
     else:
-        print (f'########### {item}')
-        split_item = item.split("/")
-        split_part_one_wanted_output = split_item[0]
-        split_part_two_wanted_output = split_item[1]
-                                                                # szukanie w main dict jaka para jest notowana
+        wanted_output_asset = (item.split("/")[0])
+        wanted_output_currency = (item.split("/")[1])
+        # Searching in main dictionary what is the quotation currency of wanted asset
         for element in main_dict:
-            if element.split("/")[0] == split_part_one_wanted_output:
-                print (f'^^^^^^^^^^^ {element}')
-                print (f'split_part_two_wanted_output: {split_part_two_wanted_output} ||||| element.split("/")[1] {element.split("/")[1]}')
-                required_ex_rate = (f'{element.split("/")[1]}/{split_part_two_wanted_output}')
-                print (required_ex_rate)
+            if element.split("/")[0] == wanted_output_asset:
+                # Defining what currency pair should be used for a recalculation of price
+                required_ex_rate = (f'{element.split("/")[1]}/{wanted_output_currency}')
                 break
+        # Calculation of asset price in wanted currency
         price_to_be_exchanged = (fetch_price(main_dict[element]))
         ex_rate_to_be_uesed = (updated_exchange_rates_dict[required_ex_rate])
         price = (price_to_be_exchanged * ex_rate_to_be_uesed)
-        print (price)
         updated_main_dict[item] = price
-        print ()
-        print ("test2")
-    print ("test3")
-print ("test4")
-                                                                # na tym etapie mam exchange rate. Wystarczy pomnożyć
-
 print (updated_main_dict)
-        
+
+# Save output_assets.txt file with prices of assets from input-wanted_result.json file. 
+try:
+    with open(standardize_file_path("output_assets.txt"), "w") as file:
+        for line in updated_main_dict:
+            price = updated_main_dict[line]
+            file.write (f'\n{line} === {round_accordingly_to_value_size(price)}')
+except Exception as e:
+        print(f"\n################\nError exporting TXT data ---------> {str(e)}\n################\n")
+
